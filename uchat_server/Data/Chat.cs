405505f775f9fd07e55211f090817cb776d763a9
Viktoriia <v380a7358mmds@gmail.com>
2025-12-06 20:12:24 +0300

    server: first version of data model

---
+++ uchat_server/Data/Chat.cs
@@ -0,0 +1,12 @@
+namespace uchat_server.Data;
+
+public class Chat
+{
+    public int Id { get; set; }
+    public bool IsGroup { get; set; } = false;
+    public string? Name { get; set; }
+    public byte[]? Avatar { get; set; }
+
+    public ICollection<ChatMember> Members { get; set; } = new List<ChatMember>();
+    public ICollection<Message> Messages { get; set; } = new List<Message>();
+}
--- uchat_server/Data/User.cs
+++ uchat_server/Data/User.cs
@@ -3,4 +3,12 @@
 public class User
 {
     public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Nickname { get; set; } = null!;
+        public string HashPassword { get; set; } = null!;
+        public bool IsOnline { get; set; } = false;
+        public byte[]? Avatar { get; set; }
+    
+        public ICollection<ChatMember> ChatMembers { get; set; } = new List<ChatMember>();
+        public ICollection<Message> Messages { get; set; } = new List<Message>();
 }
